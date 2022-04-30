4b7676aeb98db23a64ffb0653a5ac9db54148332
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-04-30 08:48:30 +0100

    Modify frontend.kv CamaraScreen and main class CameraScreen

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-import wikipedia
+import time
+from filesharer import FileShare
 import requests
 
 Builder.load_file('frontend.kv')
 
 
-class FirstScreen(Screen):
+class CameraScreen(Screen):
+
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.button_camera.text = 'Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.button_camera.text = 'Start Camera'
+        self.ids.camera.texture = None
+
+    def capture(self):
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filename = f'files/{current_time}.png'
+        self.ids.camera.export_to_png(filename)
+
+
+
+class ImageScreen(Screen):
     pass
 
 class RootWidget(ScreenManager):
