b8c14202f46d6284bc5e021f41e0c5a75a30e998
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-04-30 09:58:54 +0100

    Finish filesharer.py

--- filesharer.py
+++ filesharer.py
@@ -9,4 +9,4 @@ class FileShare:
     def share(self):
         client = Client(self.api_key)
         new_filelink = client.upload(filepath=self.filepath)
-        return new_filelink
+        return new_filelink.url
