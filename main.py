bbd3f4a0289b09d7e6f11dbe2231ae3d2162ab6c
Juan Manuel <juanmanuelcabrera.r@gmail.com>
2022-04-30 09:58:42 +0100

    Finish main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.core.clipboard import Clipboard
+import webbrowser
 import time
 from filesharer import FileShare
 import requests
@@ -22,13 +24,38 @@ class CameraScreen(Screen):
 
     def capture(self):
         current_time = time.strftime('%Y%m%d-%H%M%S')
-        filename = f'files/{current_time}.png'
-        self.ids.camera.export_to_png(filename)
-
+        #Creamos self.filepath para que no sea una variable, sino un atributo
+        #y así poder acceder a él en la función create_link()
+        self.filepath = f'files/{current_time}.png'
+        self.ids.camera.export_to_png(self.filepath)
+        #cambia de pantalla
+        self.manager.current = 'image_screen'
+        #carga la imagen en la nueva pantalla
+        self.manager.current_screen.ids.image.source = self.filepath
 
 
 class ImageScreen(Screen):
-    pass
+
+    link_message = "Create a Link First"
+
+    def create_link(self):
+        #Accede al atributo filepath de CameraScreen.capture
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        filesharer = FileShare(filepath=file_path)
+        self.url = filesharer.share()
+        self.ids.link.text = self.url
+
+    def copy_link(self):
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
 
 class RootWidget(ScreenManager):
     pass
